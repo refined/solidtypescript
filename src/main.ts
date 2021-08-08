1600e5ff92280b26a4e66dbf135209f3680df43c
Fedor <novikovfedor@gmail.com>
2021-08-08 21:53:34 +0300

    interface segregation

--- src/main.ts
+++ src/main.ts
@@ -31,12 +31,41 @@
 // processor.pay(order, "0372846");
 
 // --3-- liskov substitution
-import { Order, PaypalPaymentProcessor } from "./3_liskov_substitution";
+// import { Order, PaypalPaymentProcessor } from "./3_liskov_substitution";
+// const order = new Order();
+// order.add_item("Keyboard", 1, 50);
+// order.add_item("SSD", 1, 150);
+// order.add_item("USB cable", 2, 5);
+
+// console.log(order.total_price());
+// const processor = new PaypalPaymentProcessor();
+// processor.pay(order, "0372846");
+
+// --4-- interface segregation inheritence
+// import { Order, PaypalPaymentProcessor } from "./4_interface_segregation_inheritence";
+// const order = new Order();
+// order.add_item("Keyboard", 1, 50);
+// order.add_item("SSD", 1, 150);
+// order.add_item("USB cable", 2, 5);
+
+// console.log(order.total_price());
+// const processor = new PaypalPaymentProcessor("hi@arjancodes.com");
+// processor.auth_sms(465839);
+// processor.pay(order);
+
+// --4-- interface segregation inheritence
+import {
+    Order,
+    PaypalPaymentProcessor,
+    SmsAuthorizer,
+} from "./4_interface_segregation_segregate";
 const order = new Order();
 order.add_item("Keyboard", 1, 50);
 order.add_item("SSD", 1, 150);
 order.add_item("USB cable", 2, 5);
 
 console.log(order.total_price());
-const processor = new PaypalPaymentProcessor();
-processor.pay(order, "0372846");
+const authorizer = new SmsAuthorizer();
+authorizer.verify_code(465839);
+const processor = new PaypalPaymentProcessor("hi@arjancodes.com", authorizer);
+processor.pay(order);
