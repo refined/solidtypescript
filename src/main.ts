d5301e4193dbce4dca0ca1b5d4e39e027ff50c58
Fedor <novikovfedor@gmail.com>
2021-08-08 21:10:44 +0300

    liskov substitution

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@
 // console.log(order.total_price());
 // order.pay("debit", "0372846");
 
-// --1--
+// --1-- single responsibility
 // import { Order, PaymentProcessor } from "./1_single_responsibility";
 // const order = new Order();
 // order.add_item("Keyboard", 1, 50);
@@ -19,13 +19,24 @@
 // const processor = new PaymentProcessor();
 // processor.pay_debit(order, "0372846");
 
-// --2--
-import { Order, DebitPaymentProcessor } from "./2_open_closed";
+// --2-- open closed
+// import { Order, DebitPaymentProcessor } from "./2_open_closed";
+// const order = new Order();
+// order.add_item("Keyboard", 1, 50);
+// order.add_item("SSD", 1, 150);
+// order.add_item("USB cable", 2, 5);
+
+// console.log(order.total_price());
+// const processor = new DebitPaymentProcessor();
+// processor.pay(order, "0372846");
+
+// --3-- liskov substitution
+import { Order, PaypalPaymentProcessor } from "./3_liskov_substitution";
 const order = new Order();
 order.add_item("Keyboard", 1, 50);
 order.add_item("SSD", 1, 150);
 order.add_item("USB cable", 2, 5);
 
 console.log(order.total_price());
-const processor = new DebitPaymentProcessor();
+const processor = new PaypalPaymentProcessor();
 processor.pay(order, "0372846");
